5ad9ababc4609ebb993d6c5ee24b8d714dbc1331
AK97 <ashwinkumar97@hotmail.com>
2020-05-09 23:16:18 -0400

    initiated readme, package json. installed express & socket node modules. began webpage with stylesheet. implemented working chatbox with time indicator.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Mafia
 Mafia game
+
+Recreation of the party game "Mafia" in text-based online multiplayer form.
+A work in progress.
+By NM & AK.
+
+Run locally:
+-Clone repo
+-In terminal, run "node index.js"
+-In browser, visit localhost:69
---
+++ index.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var app = express();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+const port = 69; //port for hosting site on local system. will probably be invalidated once hosted elsewhere.
+
+function currentTime() { //returns current time in "[HH:MM:SS] " 24hr format (string)
+  var d = new Date();
+  return '['+d.toTimeString().substr(0,8)+'] ';
+}
+
+app.use('/styles',express.static(__dirname + '/styles')); //provide client with (static) stylesheets
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+
+io.on('connection', (socket) => {
+  //what to do upon new user appearing
+  //TODO: identify them, assign username, announce to game log who has arrived
+  console.log('a user connected');
+  socket.on('chat message', (msg) => {
+    var time_appended_msg = currentTime()+msg
+    console.log('message: ' + time_appended_msg); //print the chat message event
+    io.emit('chat message', time_appended_msg); //send message to everyone including sender
+  });
+  socket.on('disconnect', () => {
+  	//what to do upon new user disappearing
+  	//TODO: announce to gamelog who has disconnected
+    console.log('user disconnected');
+  });
+});
+
+http.listen(port, () => {
+  console.log('listening on *:'+port);
+});
